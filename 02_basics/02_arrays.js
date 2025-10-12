e2bf83db65b99662d7f8344a8a437370e7e80068
Virendra <160396112+virendra102@users.noreply.github.com>
2025-10-12 07:16:17 +0000

    learned array methods and objects

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,33 @@
+const mcu = ["Thor","Cap","Ironman"];
+const dc = ["Superman","Batman","flash"];
+
+// console.log(mcu)
+// console.log(dc)
+
+// mcu.push(dc);
+
+// console.log(mcu[3][1])
+
+// const allU = mcu.concat(dc);
+// console.log(allU)
+
+
+// spread operator
+
+// const allUh = [...mcu,...dc]
+// console.log(allUh)
+
+const hero = [1,2,3,[4,5,6],7,8]
+
+const heroCou = hero.flat(Infinity)
+//console.log(heroCou)
+
+console.log(Array.isArray("ranank"))
+console.log(Array.from("hitessh"))
+console.log(Array.from ({name:"hitesh"})) //interesting
+
+let score1 = 996
+let score2 = 55
+let score =343
+
+console.log(Array.of(score1,score2,score));
---
+++ 02_basics/03_objects.js
@@ -0,0 +1,18 @@
+// singleton
+
+//object literals
+
+//Object.create
+const user = {
+    name:"Viren",
+    age: 20,
+    location: "champaner",
+    "state": "Rajastan",
+    email: "raju@google.com",
+    isLoggedIN: false,
+    lastLoginDays: ["monday","saturday"]
+}
+
+console.log(user.location)
+console.log(user["location"])
+console.log(user["state"])
