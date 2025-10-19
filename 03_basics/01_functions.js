71af46dee75d146bb347d39c5bc319a93807f513
Virendra <160396112+virendra102@users.noreply.github.com>
2025-10-19 07:16:52 +0000

    func. object and array call

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -28,4 +28,42 @@ function loginUserMess(username= "ragu"){
     return `${username} just logged in`;
 }
 
-console.log(loginUserMess());
+//console.log(loginUserMess());
+
+// function calculateCarPrice(...num1){
+//     return num1;
+// }
+
+// console.log(calculateCarPrice(5,40,5))
+
+// function calculateCarPrice(value1,value2,...num1){
+//     return num1;
+// }
+
+// console.log(calculateCarPrice(5,40,5))
+
+const user ={
+    username:"virendra",
+    price: 200,
+}
+
+function handleObject(anyobject){
+    console.log(`this user named ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+//handleObject(user);
+
+handleObject({
+    username: "rashid",
+    price: 600
+})
+
+const homi = [55,65,88,25]
+
+function returnSecondValue(getArray){
+    return getArray[2];
+}
+
+//console.log(returnSecondValue(homi))
+
+console.log(returnSecondValue([66,100,89,44]))
