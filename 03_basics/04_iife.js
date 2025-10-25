e4198386c882bf36970aba1f88c999b6f3e962cc
Virendra <160396112+virendra102@users.noreply.github.com>
2025-10-25 10:35:17 +0000

    IIFE Illustration

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,16 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+
+(function kay(){
+console.log(`DB CONNECTED`);
+})();
+
+((name)=>{
+    console.log(`kuso yaro ${name}`)
+})('prakash');
+
+
+// this is iife ,
+//  it is used to direct call the function by putting () in the end of the function 
+// and also putting the function in ()
+//  " and don't forget to put ; in the end of the iife braket it will tell when to stop the function."
