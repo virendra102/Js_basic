654935bdf2da7922e75bb61cf099124aa523561c
Virendra <160396112+virendra102@users.noreply.github.com>
2025-10-31 11:14:27 +0530

    swich case flow

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,32 @@
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 5;
+
+switch (month) {
+  case 1:
+    console.log("January");
+    break;
+  case 2:
+    console.log("Febuary");
+    break;
+  case 3:
+    console.log("March");
+    break;
+  case 4:
+    console.log("April");
+    break;
+  case 5:
+    console.log("May");
+    break;
+
+  default:
+    console.log("default case match");
+    break;
+}
