deac91edec11e93f21769d7c7d4ba2ebb23954ec
Virendra <160396112+virendra102@users.noreply.github.com>
2025-10-31 05:41:46 +0000

    if conditional flow

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -12,5 +12,29 @@ const Temperature = 50;
 
 const balance = 1000;
 
-if(balance>500 ) console.log("aren't you rich?"), console.log("i think that i am just guessinit.");
+//if(balance>500 ) console.log("aren't you rich?"), console.log("i think that i am just guessinit.");
 // above example is not a good practice.
+
+// if(balance<500){
+//     console.log("less than 500")
+// }else if (balance<750){
+//     console.log("less than 750")
+// }else if (balance<1000){
+//     console.log("less than 1000");
+// }else {
+//     console.log("less than 1200");
+    
+// }
+
+const userLoggedIn = true;
+const debitCard = true;
+const loggedInFromGoogle = false;
+const loggedInFromEmail = true;
+
+if (userLoggedIn && debitCard && 5==6) {
+   // console.log("allow to buy course");
+}
+
+if (loggedInFromEmail|| loggedInFromGoogle) {
+   // console.log("user logged in");
+}
