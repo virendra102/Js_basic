819053c900a8618b862acd9730350f2c2b8abe11
Virendra <160396112+virendra102@users.noreply.github.com>
2025-10-31 06:44:47 +0000

    map and forof

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -1,5 +1,8 @@
 // for of
 
+// ["","",""]
+// [{},{},{}]
+
 const arr = [1, 2, 3, 4, 5, 6];
 
 for (const num of arr) {
@@ -11,11 +14,24 @@ for (const get of greet) {
   //console.log(`each char is ${get}`);
 }
 
-// Map
+// Maps
 
 const map = new Map();
 map.set("IN", "INDIA");
 map.set("UK", "LONDON");
 map.set("JAP", "TOKYO");
 
-console.log(map);
+//console.log(map);
+
+for (const [key,value] of map) {
+  //console.log(key ,':-', value)
+}
+
+const myObj ={
+  'game1': 'nfs',
+  'game2': 'spiderman'
+}
+
+for (const [key,value] of myObj) {
+  console.log(key ,':-', value)
+}
