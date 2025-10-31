bfdab07eb1f46bc9e9f205fd33d5f9a5e26e3f66
Virendra <160396112+virendra102@users.noreply.github.com>
2025-10-31 03:02:29 +0000

    if condition

---
+++ 04_control_flow/one.js
@@ -0,0 +1,16 @@
+// if
+const isUserLoggedIn =true;
+const Temperature = 50;
+
+// if(Temperature<30){
+//     console.log("it's cold out here")
+// } else {
+//     console.log("Hmm, An balanced Temperature may be.")
+// }
+
+// console.log("if it's more than normal than we are done");
+
+const balance = 1000;
+
+if(balance>500 ) console.log("aren't you rich?"), console.log("i think that i am just guessinit.");
+// above example is not a good practice.
