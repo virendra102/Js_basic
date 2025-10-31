4e33109f0ea8b94b2ed9c53cc9dee10f3043e515
Virendra <160396112+virendra102@users.noreply.github.com>
2025-10-31 06:45:05 +0000

    object forin

---
+++ 05_iterations/four.js
@@ -0,0 +1,30 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'c++',
+    rb: 'ruby',
+    swift : 'swift by apple'
+}
+
+for (const key in myObject) {
+    
+    //console.log(`${key} short cut is for ${myObject[key]}`)
+    
+}
+
+const programming = ['js','rb','c','java','python']
+
+for (const key in programming) {
+    //console.log(programming[key])
+    
+}
+
+const map = new Map();
+map.set("IN", "INDIA");
+map.set("UK", "LONDON");
+map.set("JAP", "TOKYO");
+
+for (const key in map) {
+    console.log(map[key]);
+    
+    
+}
