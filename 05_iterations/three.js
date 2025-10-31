8b2bf02688b192d1be62c4c4647a45e3e7da19cb
Virendra <160396112+virendra102@users.noreply.github.com>
2025-10-31 11:16:20 +0530

    Add files via upload

---
+++ 05_iterations/three.js
@@ -0,0 +1,21 @@
+// for of
+
+const arr = [1, 2, 3, 4, 5, 6];
+
+for (const num of arr) {
+  //console.log(num);
+}
+
+const greet = "hello world";
+for (const get of greet) {
+  //console.log(`each char is ${get}`);
+}
+
+// Map
+
+const map = new Map();
+map.set("IN", "INDIA");
+map.set("UK", "LONDON");
+map.set("JAP", "TOKYO");
+
+console.log(map);
---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+let i = 0;
+
+while (i <= 10) {
+  //console.log(`value of index is ${i}`);
+  i++;
+}
+
+let array = ["mulan", "elza", "shaina"];
+let arr = 0;
+while (arr < array.length) {
+  //console.log(`${array[arr]} is てんさい　人　です`);
+  arr++;
+}
+
+let score = 1;
+do {
+  //console.log(`Score is ${score}`);
+  score++;
+  //console.log(`the increment is ${score}`);
+} while (score <= 10);
