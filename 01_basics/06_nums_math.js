3e23092993bb95bc62c9deb0ca79307e29f6f657
Virendra <160396112+virendra102@users.noreply.github.com>
2025-11-14 05:08:43 +0000

    recap

---
+++ 01_basics/06_nums_math.js
@@ -0,0 +1,37 @@
+const score = 100;
+
+const balance = new Number(1000);
+// console.log(balance);
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+
+// console.log(typeof score);
+// console.log(typeof balance);
+
+const otherBalance = 1510.2323;
+console.log(otherBalance.toPrecision(4));
+
+const hundreds = 10000000;
+console.log(hundreds.toLocaleString("en-IN"));
+
+//++++++++++++++++++++++++++++++++++++++ Math +++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math.PI);
+// console.log(Math.abs(-100));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+
+// console.log(Math.random());
+// console.log(Math.floor((Math.random() * 10) + 1));
+
+// this shows the how random value is transformed into a good figure.
+// const dam = Math.random();
+// console.log(dam);
+// console.log(dam*10);
+// console.log((dam*10)+1);
+// const roger = Math.floor((dam*10)+1);
+// console.log(roger);
+
+const min = 10;
+const max = 20;
+//console.log(Math.floor((Math.random() * (max - min + 1)) + min));
