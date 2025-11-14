6f0b9f7acd44af14eb7e7663d400c5d145a59d9f
Virendra <160396112+virendra102@users.noreply.github.com>
2025-11-14 05:09:43 +0000

    data and time check

--- 01_basics/07_dates_in.js
+++ 01_basics/07_dates_in.js
@@ -1,22 +1,20 @@
 // time date
 
 let myDate = new Date();
-//console.log(myDate.toString());
+// console.log(myDate.toString());
 // console.log(myDate.toDateString());
 // console.log(myDate.toLocaleDateString());
 // console.log(myDate.toLocaleString());
-// console.log(typeof myDate);
-
-let myCreatedDate = new Date(2024,9,31,5,30,0);
 //console.log(myCreatedDate.toLocaleString());
 
 let myTimeStamp = Date.now();
 // console.log(myTimeStamp);
 // console.log(myCreatedDate.getTime());
-//console.log(Math.floor(myTimeStamp/(1000*60*60*24)));
+// console.log(Math.floor(myTimeStamp/(1000*60*60*24)));
 
 let newDate = new Date("2024-10-31");
-//console.log(newDate.getMonth());
+// console.log(newDate);
+// console.log(newDate.getMonth());
 //'${newDate.getDay()}'}
 
-console.log(newDate.toLocaleString("default", {month: "short"}))
+console.log(newDate.toLocaleString("default", {month: "long"}))
